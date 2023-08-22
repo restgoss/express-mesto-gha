37f1f8e77963e07c5d5da68893047fb348be107c
Dmitriy Glebov <120340349+restgoss@users.noreply.github.com>
2023-08-22 15:10:46 +0300

    add 404 path response

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ const cardsRouter = require('./routes/cards');
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 mongoose.connection.on('connected', () => console.log('Mongoose connected'));
 
-// Middleware для обработки JSON
 app.use(express.json());
 
-// Middleware для req.user
 app.use((req, res, next) => {
   req.user = {
     _id: '64e36c9140cfc2983e43bb4c'
@@ -20,11 +18,12 @@ app.use((req, res, next) => {
   next();
 });
 
-// Используем роутеры
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+router.use('*', (req, res) => {
+  res.status(404).send({ message: 'Not Found' });
+});
 
-// Применяем роутер
 app.use(router);
 
 app.listen(port, () => {
